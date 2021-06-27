14e8003056ec4916c03a4a290cb7e502afcd6c5d
advuianime <68636322+advuianime@users.noreply.github.com>
2021-06-27 17:02:45 +0700

    Update servers.js

--- configs/servers.js
+++ configs/servers.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
 exports.ALLOW_DOMAIN 	= [];
-exports.NumberNode 		= 10; //The number of nodes you want to play
+exports.NumberNode 		= 1; //The number of nodes you want to play
 exports.KeyCacheNode	= 'LoadBlancer';	
 exports.DomainNode 		= ['cki.name']; //Your primary domain: yourdomain.com. Then you can use: sv1.yourdomain.com, sv2.yourdomain.com
 exports.Prefix			= 'cdn'; //Prefix: sv1.yourdomain.com, sv2.yourdomain.com, etc.
